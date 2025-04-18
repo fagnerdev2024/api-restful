2475c9822e138748492d765789c9f0afee452c20
Fagner Menezes <fagnertavaresdemenezes2024@gmail.com>
2025-04-18 15:22:29 -0300

    construcao dos enums

--- src/main/java/med/voll/api/enums/MotivoCancelamento.java
+++ src/main/java/med/voll/api/enums/MotivoCancelamento.java
@@ -2,8 +2,22 @@ package med.voll.api.enums;
 
 public enum MotivoCancelamento {
 
-    PACIENTE_DESISTIU,
-    MEDICO_CANCELOU,
-    OUTROS;
+    PACIENTE_DESISTIU("Paciente desistiu"),
+    MEDICO_CANCELOU("Médico cancelou"),
+    OUTROS("Outros motivos");
 
+    private final String descricao;
+
+    MotivoCancelamento(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return name() + " - " + descricao;
+    }
 }
